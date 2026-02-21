c8aca66c75af361e77fbc19d28c211ed19f2e53a
Shubham Patel <vtshubham01@gmail.com>
2026-02-21 17:30:08 +0530

    multi entry fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        naman: resolve(__dirname, "naman-anisha.html"),
+        anisha: resolve(__dirname, "anisha-naman.html"),
+      },
+    },
+  },
+});
